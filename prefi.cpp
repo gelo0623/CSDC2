573ec372a29a1aafe9b15e9ece30ffc581b0e50c
Gelo <mapanis@gbox.adnu.edu.ph>
2026-04-16 15:22:28 +0800

    Fix loop bounds and average calculation in prefi.cpp

--- prefi.cpp
+++ prefi.cpp
@@ -20,7 +20,7 @@ int main () {
     
     cout << "===== Student Grade Report =====" << endl;
     
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 5; i++) {
     
     cout << "Student #" << i + 1 << endl;
 
@@ -34,19 +34,19 @@ cout << "Input final exam score: \n";
 cin >> exam[i].final_exam;
 
 
-exam[i].average = exam[i].midterm + exam[i].final_exam /  2;
+exam[i].average = (exam[i].midterm + exam[i].final_exam) / 2;
 
 
+}
 
-    }
-    for (int i = 1; i <= 5; i++){
-    
-    cout << "==============================================" << endl;
+cout << "==============================================" << endl;
     cout << "\nName\t\tMidterm\tFinal\tAverage\tRemark" << endl;
     cout << "----\t\t-------\t-----\t-------\t------" << endl;
-    cout << "==============================================" << endl;
+   
     
-    cout << exam[i].name << "\t" << exam[i].midterm << "\t" << exam[i].final_exam << "\t" << exam[i].average
+    for (int i = 0; i < 5; i++){
+    
+    cout << exam[i].name << "\t\t" << exam[i].midterm << "\t" << exam[i].final_exam << "\t" << exam[i].average
  << "\t" << (exam[i].average >= 75 ? "Passed" : "Failed") << endl;
     
     }
