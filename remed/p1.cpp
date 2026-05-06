54b119d2b47b53185dc7fbee6434246ef0a79224
Gelo <mapanis@gbox.adnu.edu.ph>
2026-05-06 16:41:22 +0800

    Create p1.cpp

---
+++ remed/p1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+
+	int n, x;
+	int sum = 0;
+	double average;
+	int passcount = 0;
+
+	cout << "Enter number of elements: ";
+	cin >> n;
+
+
+	int arr[n];
+
+	cout << "Enter elements: ";
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+
+		sum += arr[i];
+		if (arr[i] >= 60) {
+			passcount++;
+		}
+
+
+	}
+
+	average = (double)sum / n;
+
+
+	int largest = arr[0];
+
+
+	for (int i = 1; i < n; i++) {
+		if (arr[i] > largest) {
+			largest = arr[i];
+		}
+	}
+
+	int smallest = arr[0];
+
+	for (int i = 1; i < n; i++) {
+		if (arr[i] < smallest) {
+			smallest = arr[i];
+		}
+	}
+
+
+
+
+
+
+	cout << largest << endl;
+	cout << smallest << endl;
+	cout << average << endl;
+	cout << passcount << endl;
+
+
+
+
+
+	return 0;
+}
