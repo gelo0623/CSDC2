51901593dee3880c9aa196dcfa94a9239db48dc7
Gelo <mapanis@gbox.adnu.edu.ph>
2026-05-06 15:29:26 +0800

    Create 8.cpp

---
+++ remed/8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int sum(int n){
+    if (n == 0)
+    return 0;
+    
+    return (n % 10) + sum(n / 10);
+}
+
+int f(int n) {
+if (n < 10)
+return n;
+
+return f(sum(n));
+
+}
+
+int main () {
+    int n;
+    
+    
+    cout << "Enter numbers to flatten: "'
+    cin >> n;
+    
+    cout << f(n);
+    
+    
+    return 0;
+}
