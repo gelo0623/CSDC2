8deb5d5139b606f4f274b75758eeb465b531a51a
Gelo <mapanis@gbox.adnu.edu.ph>
2026-05-08 01:43:06 +0800

    Update p3.cpp

--- remed/p3.cpp
+++ remed/p3.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 int main() {
+
     vector<string> list;  
+    
     string command;
     
     cout << "ADD PRODUCTS (INPUT ADD first then product)" << endl;
@@ -15,28 +18,48 @@ int main() {
         if (command == "DONE") {
             break;
 
-        } else if (command == "ADD") {
+        } 
+        
+        else if (command == "ADD") {
             string item;
+            
             cin >> item;
             list.push_back(item);      
 
-        } else if (command == "REMOVE") {
+        } 
+        
+        else if (command == "REMOVE") {
             if (list.empty()) {
+                
                 cout << "List is empty." << endl;
-            } else {
+            }
+            
+            else {
                 list.pop_back();
             }
 
-        } else if (command == "COUNT") {
+            
+        } 
+        
+        else if (command == "COUNT") {
             cout << list.size() << endl;
 
-        } else if (command == "SHOW") {
+        } 
+        
+        else if (command == "SHOW") {
+            
             for (int i = 0; i < list.size(); i++) {
                 cout << i + 1 << ". " << list[i] << endl;
+                
             }
+
+            
         }
+        
     }
 
     cout << "Final list has " << list.size() << " item(s)." << endl;
+    
     return 0;
+    
 }
