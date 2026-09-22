aa67e86d5f140c7b7a705f6b2f1898507ec61af4
Gelo <mapanis@gbox.adnu.edu.ph>
2026-09-23 00:23:38 +0800

    Update circularlinkedlist.cpp

--- dsa/circularlinkedlist.cpp
+++ dsa/circularlinkedlist.cpp
@@ -26,24 +26,26 @@ void insertNode(NodePtr& Rear, int item) { //making the block
     }
 
     // Start at the first node
-    Prev = Rear; 
-    Cur = Rear->next;
+    Prev = Rear; // makes the rear the previous node // so like the cur to prev
+    Cur = Rear->next; // the next of the previous makes it the current
 
     // Find the correct position
     do{
-        if(item <= Cur->data)
+        if(item <= Cur->data) // keeps looping until the data matches the current data position
             break;
-        Prev = Cur;
-        Cur = Cur->next;
-    }while (Cur != Rear->next);
+
+        // runs if its false to the statement
+        Prev = Cur; // makes the current as the previous node
+        Cur = Cur->next; // mpoints the cur to the next node and make it as the cur / moves to one more node
+    }while (Cur != Rear->next); // did the cur gets back to line 1/ after traversing to the whole list back to the start
 
     // Insert the new node
-    New->next = Cur;
-    Prev->next = New;
+    New->next = Cur; // new->next holds the copy of the cur data node it has -> so that the cur moves to the next so 5 becoms 7
+    Prev->next = New; // prev was 3 and now points to next which is 5
 
     // If inserted after Rear, update Rear
-    if (item > Rear->data) {
-        Rear = New;
+    if (item > Rear->data) { //rear holds theh largest so checks is the value bigger than the current largest
+        Rear = New; // and if yes it moves the rear to the new largest
     }
 }
 
@@ -54,35 +56,36 @@ void deleteNode(NodePtr& Rear, int item) {
     NodePtr Cur, Prev;
 
     // Empty list
-    if (Rear == NULL) {
+    if (Rear == NULL) { //if the rear is pointinf to nothing
         cout << "Trying to delete empty list" << endl;
         return;
     }
 
     // Start at the first node
-    Prev = Rear;
-    Cur = Rear->next;
+    Prev = Rear; // creating node // makes the current rear as the previousnode
+    Cur = Rear->next; // the next of the  rear makes it as the current node
 
     // Find the node
     do {
-        if (item <= Cur->data)
+        if (item <= Cur->data) //keeps looping until it matches the data current position
             break;
 
-        Prev = Cur;
-        Cur = Cur->next;
+        // runs if false statement
+        Prev = Cur; // makes the curent node as the previous
+        Cur = Cur->next; //current node is pointing to the next and makes that the currrent
 
-    } while (Cur != Rear->next);
+    } while (Cur != Rear->next); // if cur does not yet stilll matchhes the rear position it keeps looping
 
 
     // Item was not found
-    if (Cur->data != item) {
+    if (Cur->data != item) { //if the current data is not on the item
         cout << "Data Not Found" << endl;
         return;
     }
 
 
     // Only one node in the list
-    if (Cur == Prev) {
+    if (Cur == Prev) { // if theres only 1 node, set rear to NULL and delete the node
         Rear = NULL;
         delete Cur;
         return;
@@ -90,32 +93,32 @@ void deleteNode(NodePtr& Rear, int item) {
 
 
     // If deleting the Rear node
-    if (Cur == Rear)
+    if (Cur == Rear) //makes the previous node the rear when deleting
         Rear = Prev;
 
 
     // Remove Cur
-    Prev->next = Cur->next;
+    Prev->next = Cur->next; //unlinks the memory
 
-    delete Cur;
+    delete Cur; //delete
 }
 
 
 // Display the circular linked list
 void print(NodePtr Rear) {
 
-    if (Rear == NULL) {
+    if (Rear == NULL) { //if list  is empyt
         cout << "List is empty" << endl;
         return;
     }
 
-    NodePtr Cur = Rear->next;
+    NodePtr Cur = Rear->next; // makes the next of the rear the current node
 
     do {
-        cout << Cur->data << " ";
-        Cur = Cur->next;
+        cout << Cur->data << " "; //rint the current data
+        Cur = Cur->next; //moves the current node
 
-    } while (Cur != Rear->next);
+    } while (Cur != Rear->next); //keeps repeating until reaches the position original //keeps printing 
 
     cout << endl;
 }
